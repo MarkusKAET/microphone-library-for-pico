22c6e7873115e18677e126cbc15722e001481083
Eric Heim <854546+ericheim@users.noreply.github.com>
2024-01-09 11:15:24 +0100

    Export C Symbols
    Add extern C export directives to use the analog_microphone in
    a c++ program.

--- src/include/pico/analog_microphone.h
+++ src/include/pico/analog_microphone.h
@@ -2,11 +2,15 @@
  * Copyright (c) 2021 Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: Apache-2.0
- * 
+ *
  */
 
 #ifndef _PICO_ANALOG_MICROPHONE_H_
 #define _PICO_ANALOG_MICROPHONE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef void (*analog_samples_ready_handler_t)(void);
 
@@ -26,5 +30,9 @@ void analog_microphone_stop();
 void analog_microphone_set_samples_ready_handler(analog_samples_ready_handler_t handler);
 
 int analog_microphone_read(int16_t* buffer, size_t samples);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
